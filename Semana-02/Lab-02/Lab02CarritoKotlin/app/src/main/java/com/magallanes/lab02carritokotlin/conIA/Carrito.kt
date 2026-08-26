489c135d1c63537b18536110c37eecb2f2cd736c
DiegoMagallanes11 <magallaneslda@gmail.com>
2026-08-26 14:04:06 -0500

    feat: agregar abstraccion de productos

--- Semana-02/Lab-02/Lab02CarritoKotlin/app/src/main/java/com/magallanes/lab02carritokotlin/conIA/Carrito.kt
+++ Semana-02/Lab-02/Lab02CarritoKotlin/app/src/main/java/com/magallanes/lab02carritokotlin/conIA/Carrito.kt
@@ -29,3 +29,31 @@ fun main() {
     println("Cliente: ${cliente.obtenerNombre()}")
     println("Documento: ${cliente.obtenerDocumento()}")
 }
+interface Calculable {
+    fun calcularImporte(): Double
+}
+
+abstract class ProductoBase(
+    protected val nombre: String,
+    protected val precioBase: Double
+) : Calculable {
+
+    init {
+        require(nombre.isNotBlank())
+        require(precioBase >= 0)
+    }
+
+    fun obtenerNombre(): String {
+        return nombre
+    }
+
+    fun obtenerPrecioBase(): Double {
+        return precioBase
+    }
+
+    abstract override fun calcularImporte()
+
+    override fun toString(): String {
+        return "$nombre - S/ ${String.format("%.2f", precioBase)}"
+    }
+}
