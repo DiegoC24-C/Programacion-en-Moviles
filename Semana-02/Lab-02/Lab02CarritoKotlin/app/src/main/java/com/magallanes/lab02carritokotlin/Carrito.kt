f167e77d7e412ae3aa32a09b712d9b4aad55bf70
DiegoMagallanes11 <magallaneslda@gmail.com>
2026-08-26 10:23:07 -0500

    Proyecto inicial con menú del carrito

---
+++ Semana-02/Lab-02/Lab02CarritoKotlin/app/src/main/java/com/magallanes/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.magallanes.lab02carritokotlin
+
+fun main() {
+    println("=========================================")
+    println("    CARRITO DE COMPRAS - TIENDA TECSUP   ")
+    println("=========================================")
+}
