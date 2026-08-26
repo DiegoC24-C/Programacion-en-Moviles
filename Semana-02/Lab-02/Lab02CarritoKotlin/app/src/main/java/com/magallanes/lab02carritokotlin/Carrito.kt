db2197bef87e004b909cea94d67fed7311ff5947
DiegoMagallanes11 <magallaneslda@gmail.com>
2026-08-26 13:27:34 -0500

    Agregar data class Producto con atributos val y var

--- Semana-02/Lab-02/Lab02CarritoKotlin/app/src/main/java/com/magallanes/lab02carritokotlin/Carrito.kt
+++ Semana-02/Lab-02/Lab02CarritoKotlin/app/src/main/java/com/magallanes/lab02carritokotlin/Carrito.kt
@@ -1,5 +1,11 @@
 package com.magallanes.lab02carritokotlin
 
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
+
 fun main() {
     println("==")
     println("CARRITO DE COMPRAS")
