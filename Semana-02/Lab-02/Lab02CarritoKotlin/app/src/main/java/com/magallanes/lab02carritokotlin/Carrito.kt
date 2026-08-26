622768efd1da44df867882a67247499349699ed3
DiegoMagallanes11 <magallaneslda@gmail.com>
2026-08-26 13:33:09 -0500

    Agregar cliente y lista de productos al carrito

--- Semana-02/Lab-02/Lab02CarritoKotlin/app/src/main/java/com/magallanes/lab02carritokotlin/Carrito.kt
+++ Semana-02/Lab-02/Lab02CarritoKotlin/app/src/main/java/com/magallanes/lab02carritokotlin/Carrito.kt
@@ -1,14 +1,22 @@
 package com.magallanes.lab02carritokotlin
 
-data class Producto(
-    val nombre: String,
-    val precio: Double,
-    var cantidad: Int
-)
-
 fun main() {
     println("==")
     println("CARRITO DE COMPRAS")
     println("TIENDA TECSUP")
     println("==")
+
+    val nombreCliente = "Jose Magallanes"
+    val carrito = mutableListOf<Producto>()
+    println("Cliente: $nombreCliente\n")
+
+    carrito.add(Producto("Laptop HP", 2500.0, 1))
+    carrito.add(Producto("Mouse Logitech", 45.5, 2))
+    carrito.add(Producto("Audifonos Sony", 120.0, 1))
+    carrito.add(Producto("USB Kingston 64GB", 25.0, 3))
+
+    for (producto in carrito) {
+        println("Producto agregado: ${producto.nombre}")
+    }
+    println()
 }
