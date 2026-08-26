c6f573803db9d49c3d7f9569becc6fa00dd4505a
DiegoMagallanes11 <magallaneslda@gmail.com>
2026-08-26 14:03:38 -0500

    feat: crear estructura inicial del carrito

--- Semana-02/Lab-02/Lab02CarritoKotlin/app/src/main/java/com/magallanes/lab02carritokotlin/conIA/Carrito.kt
+++ Semana-02/Lab-02/Lab02CarritoKotlin/app/src/main/java/com/magallanes/lab02carritokotlin/conIA/Carrito.kt
@@ -1,2 +1,31 @@
 package com.magallanes.lab02carritokotlin.conIA
 
+class Cliente(
+    private val nombre: String,
+    private val documento: String
+) {
+
+    fun obtenerNombre(): String {
+        return nombre
+    }
+
+    fun obtenerDocumento(): String {
+        return documento
+    }
+
+    override fun toString(): String {
+        return "$nombre - DNI: $documento"
+    }
+}
+
+fun main() {
+
+    val cliente = Cliente(
+        "Diego Magallanes",
+        "76543210"
+    )
+
+    println("CARRITO DE COMPRAS TIENDA TECSUP")
+    println("Cliente: ${cliente.obtenerNombre()}")
+    println("Documento: ${cliente.obtenerDocumento()}")
+}
