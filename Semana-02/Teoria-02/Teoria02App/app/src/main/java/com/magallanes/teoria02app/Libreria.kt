5478096b6c4a162f06f0d68cdb04d1525adf3dd3
DiegoMagallanes11 <magallaneslda@gmail.com>
2026-08-30 22:28:06 -0500

    Creación del  archivo principal

---
+++ Semana-02/Teoria-02/Teoria02App/app/src/main/java/com/magallanes/teoria02app/Libreria.kt
@@ -0,0 +1,8 @@
+package com.magallanes.teoria02app
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println("--- SISTEMA DE CONTROL DE MULTAS DE BIBLIOTECA ---")
+}git add .
